ff63c1f42f4bc23bf4cb6dcbd936e2b9936bb7bf
August Foreman <47118712+madebyaug@users.noreply.github.com>
2025-06-16 01:16:14 -0400

    another tough one

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const appElement = document.querySelector("#app"); // Cache #app
+
 // UI DESIGN
 // create the headers, inputs, and, buttons
 const appH1 = document.createElement("h1");
@@ -20,6 +22,7 @@ for (let i = 0; i < 3; i++) {
   appDiv.append(appButton);
 }
 
+// Gives each Button a Class and Title
 const addNum = appDiv.querySelector("button:nth-of-type(1)");
 addNum.textContent = "Add Number";
 addNum.className = "addNumBtn";
@@ -32,74 +35,132 @@ const sortAll = appDiv.querySelector("button:nth-of-type(3)");
 sortAll.textContent = "Sort All";
 sortAll.className = "sortAllBtn";
 
+// Creates mainInput
 const appInput = document.createElement("input");
 appInput.style.border = "1px solid black";
 appInput.className = "mainInput";
 document.querySelector("#app").append(appInput);
 
-// Inside parent #app, creates 3x Div with a H2 and an Input element
-for (let i = 0; i < 3; i++) {
-  const appDisplayDiv = document.createElement("div");
-  const appDisplayH2 = document.createElement("h2");
-  const appDisplayInput = document.createElement("Input");
+// Creates a Bank, Odds, and Even Arry
+let bankTitles = ["Bank", "Odds", "Evens"];
 
-  // just had to lookup how to add Class to elements this - hopefully i'll remember ;)
+// Inside parent #App, creates 3x DIV with a H2 and an Input element
+for (let i = 0; i < 3; i++) {
+  // console.log("Inside Loop");
+  const appDisplayDiv = document.createElement("div"); // Basic div for each bank
   appDisplayDiv.classList.add("divInputs");
   appDisplayDiv.style.border = "1px solid black";
   appDisplayDiv.style.backgroundColor = "lightgrey";
-  appDisplayInput.style.backgroundColor = "lightgrey";
 
-  document.querySelector("#app").appendChild(appDisplayDiv);
-  appDisplayDiv.append(appDisplayH2);
-  appDisplayDiv.append(appDisplayInput);
+  // Created DIV and H2 structure
+  appDisplayDiv.innerHTML = `
+    <h2>${bankTitles[i]}</h2>
+    <div style="background-color: lightgrey;"></div>
+  `;
+
+  appElement.appendChild(appDisplayDiv);
+
+  // Setup INPUT Id or className directly.
+  const bankContainer = appDisplayDiv.querySelector("div");
+  bankContainer.id = bankTitles[i] + "Container";
 }
-
-// I had this in the loop above for about 2 hours couldn't deterrmine why it wasn't working
-const inputs = document.querySelectorAll(".divInputs");
-inputs[0].querySelector("input").className = "bankInput";
-inputs[1].querySelector("input").className = "oddInput";
-inputs[2].querySelector("input").className = "evenInput";
-
-// Inside each #app div element, Add text to each individual h2
-document.querySelector("#app div:nth-of-type(2) h2").textContent = "Bank";
-document.querySelector("#app div:nth-of-type(3) h2").textContent = "Odds";
-document.querySelector("#app div:nth-of-type(4) h2").textContent = "Evens";
 
 // BACKEND
 
-const mainInput = document.querySelector(".mainInput");
-const bankInput = document.querySelector(".bankInput");
-const oddInput = document.querySelector(".oddInput");
-const evenInput = document.querySelector(".evenInput");
+const mainInput = document.querySelector(".mainInput"); // Selector
+const bankInput = document.getElementById("bankInput");
+const oddInput = document.getElementById(".oddInput");
+const evenInput = document.getElementById(".evenInput");
+
+// TODO:  Empty these arrays at the end.
+// This will store the value
+const storedBankValues = [2, 3, 44, 23, 5];
+const resetValue = [];
+
+// Defines "addNumBtn"
 const addNumBtn = document.querySelector(".addNumBtn");
-const sortNumBtn = document.querySelector(".sortNumBtn");
-const sortAllBtn = document.querySelector(".sortAllBtn");
 
 //  When "addNumBtn" is clicked,
 addNumBtn.addEventListener("click", () => {
-  // and a Number in the "mainInput" field
-  const getValue = mainInput.value;
-  const storedBankValue = "";
-
-  if (!isNaN(getValue)) {
-    bankInput.value = getValue;
-  } else {
+  // TODO: What happens if the mainInput is not a number?
+  // If mainInput new value is NaN
+  if (isNaN(mainInput.value)) {
+    // Warn user of error with red mainInput border
     mainInput.style.border = "1px solid red";
+    alert(`You didn't enter a Number`);
+  } else {
+    // If it is a Number, move new value to Bank
+    const getValue = Number(mainInput.value); // Raw dom data is always a string to begin with
+    storedBankValues.push(getValue);
   }
-  storedBankValue.setItem("storeValue", bankInput.value);
+  // TODO: whenever you click addNumBtn, clear the div first before rendering the contents.
+  // reset mainInput into empty Arry
+  mainInput.value = resetValue;
+
+  // Clear the div of its contents before rendering, so that you dont get duplicates.
+  let bankContainer = document.getElementById("BankContainer");
+  bankContainer.textContent = "";
+
+  // Adds space between Numbers
+  for (let i = 0; i < storedBankValues.length; i++) {
+    bankContainer.textContent += ", " + storedBankValues[i];
+    console.log(bankContainer.textContent);
+  }
 });
 
-// //  When "sortNumBtn" is clicked,
-// addNumBtn.addEventListener("click", () => {
-//   // Get the first Number in the "mainInput" field
-//   const getValue = mainInput.value[0];
-//   const storedEvenValue = "";
-//   const storedOddValue = "";
+// Defines "sortAllBtn"
+const sortAllBtn = document.querySelector(".sortAllBtn");
 
-//   if (!isNaN(value)) {
-//     bankInput.value = value;
-//   } else {
-//     mainInput.style.border = "1px solid red";
-//   }
-//   storedBankValue.setItem("storeValue", bankInput.value);
-// });
+// When "sortAllBtn" is clicked,
+sortAllBtn.addEventListener("click", () => {
+  // TODO: Clear the containers for even and odd before rendering the numbers, to prevent duplicates.
+
+  let EvensContainer = document.getElementById("EvensContainer");
+  EvensContainer.textContent = "";
+
+  let OddsContainer = document.getElementById("OddsContainer");
+  OddsContainer.textContent = "";
+
+  // Defines mainInput
+  const getValue = mainInput.value;
+  // we need to create a loop of each arry number
+  for (let i = 0; i < storedBankValues.length; i++) {
+    // Modulo works by giving the remainder of whatever numbers. example 3 % 2, the remainder is 1 ; there it is odd.
+    // You know a number is even, because it will result in a 0
+
+    // If the number is even,
+    if (storedBankValues[i] % 2 === 0) {
+      // put in even container
+      EvensContainer.textContent += " " + storedBankValues[i];
+      // Otherwise, put in the odd container
+    } else {
+      OddsContainer.textContent += " " + storedBankValues[i];
+    }
+  }
+  getValue = resetValue;
+});
+
+// Defines "sortNumBtn"
+const sortNumBtn = document.querySelector(".sortNumBtn");
+
+// When "sortNumBtn" is clicked,
+sortNumBtn.addEventListener("click", () => {
+  // Reset Containers
+  let EvensContainer = document.getElementById("EvensContainer");
+  EvensContainer.textContent = "";
+
+  let OddsContainer = document.getElementById("OddsContainer");
+  OddsContainer.textContent = "";
+
+  // Defines mainInput
+  const getValue = mainInput.value;
+  console.log(getValue);
+  // If the number is even,
+  if (storedBankValues[0] % 2 === 0) {
+    // put in even container
+    EvensContainer.textContent = storedBankValues[0];
+  } else {
+    // Otherwise, put in the odds container
+    OddsContainer.textContent = storedBankValues[0];
+  }
+});
